1140fca3dd19d9bc24e73bcc38d9c97af138ea46
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-07 08:26:07 +0300

    Anagram Detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word.lower()
+    
+    def match(self, word_list):
+        matches=[]
+
+        for candidate in word_list:
+            if candidate.lower()==self.word:
+                continue
+            if sorted(candidate.lower())==sorted(self.word):
+                matches.append(candidate)
+        return matches
+
